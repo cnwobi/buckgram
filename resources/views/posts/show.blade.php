dd3fd085bcb6f8a8515d020190689750f5d0c3d7
cnwobi <c.nwobi@outlook.com>
2019-08-10 01:54:31 +1000

    before setting default profile picture for blade

---
+++ resources/views/posts/show.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+            <div class="col-8 ">
+                    <img class= "w-100" src="/storage/{{$post->image}}" alt="">
+            </div>
+            <div class="col-4">
+                <h3>{{$post->user->username}}</h3>
+                <div><i>{{$post->caption}}</i></div>
+            </div>
+    </div>
+        
+</div>
+@endsection
